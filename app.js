7ba7d9da0a9da48467b28512d635fd4e2b6cb8e6
NahuelPalladini <palladininahuel@gmail.com>
2023-10-09 18:36:13 -0300

    Agrega Primera Seccion Y Articulos

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const PORT = 3030;
 const app = express();
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
 app.listen(PORT, console.log(`Server running in http://localhost:${PORT}`));
