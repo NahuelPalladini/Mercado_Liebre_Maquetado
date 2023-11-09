d01efc86cd5e023f4b0d8b1dcc5f87f5f732066f
NahuelPalladini <palladininahuel@gmail.com>
2023-11-09 17:24:58 -0300

    Register en proceso

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
 
 app.listen(PORT, console.log(`Server running in http://localhost:${PORT}`));
 
