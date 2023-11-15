351a0022119f7fe2408c4fd48e546ff3e923f10c
NahuelPalladini <palladininahuel@gmail.com>
2023-11-15 17:47:35 -0300

    Efectos

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
 
 app.listen(PORT, console.log(`Server running in http://localhost:${PORT}`));
 
